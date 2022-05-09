9003248e8c43e25b2b6365c3085f06ba35b45976
Arthur E. Jones <PartyLich@gmail.com>
2022-05-09 14:03:26 -0500

    feat: add 1.1 solution

--- day01/d01.go
+++ day01/d01.go
@@ -3,5 +3,12 @@ package day01
 // PartOne counts the number of times a depth measurement increases from the previous measurement.
 // (There is no measurement before the first measurement.)
 func PartOne(depths []uint) uint {
-	return 0
+	var count uint = 0
+	for i, depth := range depths[1:] {
+		if depth > depths[i] {
+			count += 1
+		}
+	}
+
+	return count
 }
