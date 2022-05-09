8154dd8412d24efceddc33e3862b5e833897e2ba
Arthur E. Jones <PartyLich@gmail.com>
2022-05-09 18:14:43 -0500

    feat: add 1.2 solution

--- day01/d01.go
+++ day01/d01.go
@@ -20,12 +20,40 @@ func PartOne(depths []uint) uint {
 	return count
 }
 
+func sum(values []uint) uint {
+	var s uint = 0
+	for _, v := range values {
+		s += v
+	}
+
+	return s
+}
+
+// PartTwo counts the number of times the sum of measurements in each 3 value sliding window
+// increases from the previous sum.
 func PartTwo(depths []uint) uint {
-  return 0
+	var count uint = 0
+
+	end := len(depths) - 3
+	// day one and I already deeply miss Option types
+	var prev *uint
+	for i := range depths[:end] {
+		if prev == nil {
+			prev = new(uint)
+			*prev = sum(depths[i : i+3])
+		}
+		next := sum(depths[i+1 : i+4])
+		if *prev < next {
+			count += 1
+		}
+		*prev = next
+	}
+
+	return count
 }
 
 var Solution = runner.Solution{
 	Parse: func(i string) (interface{}, error) { return Parse(i) },
 	One:   func(i interface{}) interface{} { return PartOne(i.([]uint)) },
-	Two:   func(i interface{}) interface{} { return runner.Unimpl(i) },
+	Two:   func(i interface{}) interface{} { return PartTwo(i.([]uint)) },
 }
