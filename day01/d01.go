049176c0859b5811ee6c3d2cf12b32a71282f4aa
Arthur E. Jones <PartyLich@gmail.com>
2022-05-09 14:02:04 -0500

    test: add part one test

---
+++ day01/d01.go
@@ -0,0 +1,7 @@
+package day01
+
+// PartOne counts the number of times a depth measurement increases from the previous measurement.
+// (There is no measurement before the first measurement.)
+func PartOne(depths []uint) uint {
+	return 0
+}
