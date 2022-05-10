0a92219e65b0700b72b98a3f0b45fcd66bf9bc79
Arthur E. Jones <PartyLich@gmail.com>
2022-05-10 17:15:32 -0500

    feat: add 2.1 solution

--- day02/d02.go
+++ day02/d02.go
@@ -1,8 +1,11 @@
 package day02
 
 import (
-	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 
+	"github.com/partylich/advent2021/parse"
 	"github.com/partylich/advent2021/runner"
 )
 
@@ -19,12 +22,68 @@ type command struct {
 	val int
 }
 
-func Parse(in string) ([]command, error) {
-	return nil, errors.New("parse not implemented")
+type pos struct {
+	horz  int
+	depth int
+}
+
+// parseCom parses a command list from a string
+func parseCom(in string) ([]command, error) {
+	lines := parse.Lines(in)
+	commands := make([]command, len(lines))
+
+	for idx, line := range lines {
+		parts := strings.Split(line, " ")
+		dirStr, v := parts[0], parts[1]
+
+		val, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, fmt.Errorf("unable to parse command from %v: %w", line, err)
+		}
+
+		var direction dir
+
+		switch dirStr {
+		case "forward":
+			direction = Forward
+		case "down":
+			direction = Down
+		case "up":
+			direction = Up
+		default:
+			return nil, fmt.Errorf("unable to parse command from %v: %w", line, err)
+		}
+
+		commands[idx] = command{
+			direction,
+			val,
+		}
+	}
+
+	return commands, nil
+}
+
+// PartOne calculates the horizontal position and depth after following the planned course.
+// Returns final horizontal position multiplied by final depth
+func PartOne(commands []command) int {
+	var pos pos
+
+	for _, v := range commands {
+		switch v.dir {
+		case Forward:
+			pos.horz += v.val
+		case Down:
+			pos.depth += v.val
+		case Up:
+			pos.depth -= v.val
+		}
+	}
+
+	return pos.horz * pos.depth
 }
 
 var Solution = runner.Solution{
-	Parse: func(i string) (interface{}, error) { return Parse(i) },
-	One:   func(i interface{}) interface{} { return runner.Unimpl(i) },
+	Parse: func(i string) (interface{}, error) { return parseCom(i) },
+	One:   func(i interface{}) interface{} { return PartOne(i.([]command)) },
 	Two:   func(i interface{}) interface{} { return runner.Unimpl(i) },
 }
