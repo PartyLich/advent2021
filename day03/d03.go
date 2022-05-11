63ca7ce7424a880f409a45b09b40ebb81d97618c
Arthur E. Jones <PartyLich@gmail.com>
2022-05-11 15:06:06 -0500

    feat: add 3.2 solution

--- day03/d03.go
+++ day03/d03.go
@@ -2,6 +2,7 @@
 package day03
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/partylich/advent2021/parse"
@@ -51,15 +52,73 @@ func PartOne(in []string) int {
 	return gamma * epsilon
 }
 
+// partition splits a list of strings representing binary numbers based on the
+// value of digit at place
+func partition(list []string, place int) ([]string, []string) {
+	var (
+		temp0 []string
+		temp1 []string
+	)
+
+	for _, num := range list {
+		// we definitely have only '0' and '1' in the strings, no utf-8 runes
+		if string(num[place]) == "0" {
+			temp0 = append(temp0, num)
+		} else {
+			temp1 = append(temp1, num)
+		}
+	}
+
+	return temp0, temp1
+}
+
 // PartTwo multiplies the oxygen generator rating by the CO2 scrubber rating
 func PartTwo(in []string) int {
-	return 0
+	oxy := in
+	co2 := in
+
+	for i := 0; len(oxy) > 1; i++ {
+		temp0, temp1 := partition(oxy, i)
+
+		if len(temp0) > len(temp1) {
+			// most common is 0
+			oxy = temp0
+		} else {
+			// most common is 1
+			oxy = temp1
+		}
+	}
+
+	for i := 0; len(co2) > 1; i++ {
+		temp0, temp1 := partition(co2, i)
+
+		if len(temp0) > len(temp1) {
+			// most common is 0
+			co2 = temp1
+		} else {
+			// most common is 1
+			co2 = temp0
+		}
+	}
+
+	const binary int = 2
+	const size int = 64
+	oxyRate, err := strconv.ParseInt(oxy[0], binary, size)
+	if err != nil {
+		panic(err)
+	}
+	co2Rate, err := strconv.ParseInt(co2[0], binary, size)
+	if err != nil {
+		panic(err)
+	}
+
+	return int(oxyRate * co2Rate)
 }
 
 func Solution() runner.Solution {
 	return runner.Solution{
 		Parse: func(i string) (interface{}, error) { return Parse(i), nil },
 		One:   func(i interface{}) interface{} { return PartOne(i.([]string)) },
-		Two:   runner.Unimpl,
+		Two:   func(i interface{}) interface{} { return PartTwo(i.([]string)) },
 	}
 }
