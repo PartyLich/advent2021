fdcb18e82c58ddf2e7b4d56672827cd0dea8ef46
Arthur E. Jones <PartyLich@gmail.com>
2022-05-12 15:01:20 -0500

    feat: add 4.1 data types

--- day04/d04.go
+++ day04/d04.go
@@ -6,6 +6,45 @@ import (
 	"github.com/partylich/advent2021/runner"
 )
 
+const gridSize int = 5
+
+// int makes more sense, but i dont need them parsed to do comparisons
+type board = [][]string
+
+type player struct {
+	board board
+	// max number of filled spaces in any row or column
+	max int
+	// current sum of marked spaces in each row
+	rowSum [gridSize]int
+	// current sum of marked spaces in each col
+	colSum [gridSize]int
+	// marked status of each space on board
+	marked [gridSize][gridSize]bool
+}
+
+func newPlayer(board board) player {
+	var (
+		rowSum [gridSize]int
+		colSum [gridSize]int
+		marked [gridSize][gridSize]bool
+	)
+
+	return player{
+		board,
+		0,
+		rowSum,
+		colSum,
+		marked,
+	}
+}
+
+// bingo represents a bingo game state
+type bingo struct {
+	draws   []string
+	players []player
+}
+
 var Parse = parse.Lines
 
 // PartOne
