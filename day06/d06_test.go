246aa0bda37b0dde697af9b1aec2a006d28d1e87
Arthur E. Jones <PartyLich@gmail.com>
2022-05-15 17:35:55 -0500

    test: add day 6 benchmarks

--- day06/d06_test.go
+++ day06/d06_test.go
@@ -1,6 +1,10 @@
 package day06
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"testing"
+)
 
 const ex string = `3,4,3,1,2`
 
@@ -44,5 +48,43 @@ func TestPartTwo(t *testing.T) {
 	have := PartTwo(in)
 	if have != want {
 		t.Fatalf("PartTwo(%v) == %v, want %v", in, have, want)
+	}
+}
+
+func BenchmarkPartOne(b *testing.B) {
+	fileName := fmt.Sprintf("../input/%v.txt", "06")
+	input, err := os.ReadFile(fileName)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	in, err := parseFish(string(input))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		PartOne(in)
+	}
+}
+
+func BenchmarkPartTwo(b *testing.B) {
+	fileName := fmt.Sprintf("../input/%v.txt", "06")
+	input, err := os.ReadFile(fileName)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	in, err := parseFish(string(input))
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+
+	for i := 0; i < b.N; i++ {
+		PartTwo(in)
 	}
 }
