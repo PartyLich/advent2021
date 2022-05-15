d7c61695f6ad9997392fbeb4feacb89fe1c0214f
Arthur E. Jones <PartyLich@gmail.com>
2022-05-15 16:37:44 -0500

    feat: parse 6.1 fish input

--- day06/d06.go
+++ day06/d06.go
@@ -2,11 +2,28 @@
 package day06
 
 import (
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/partylich/advent2021/runner"
 )
 
 func parseFish(in string) ([]int, error) {
+	fishStr := strings.Split(strings.TrimSpace(in), ",")
 	result := make([]int, 9)
+
+	for _, v := range fishStr {
+		n, err := strconv.Atoi(v)
+		if err != nil {
+			return []int{}, err
+		}
+		if n > 8 {
+			return []int{}, fmt.Errorf("input outside of valid range (0,8): %v", n)
+		}
+
+		result[n] += 1
+	}
 
 	return result, nil
 }
--- day06/d06_test.go
+++ day06/d06_test.go
@@ -4,6 +4,21 @@ import "testing"
 
 const ex string = `3,4,3,1,2`
 
+func TestParse(t *testing.T) {
+	have, err := parseFish(ex)
+	if err != nil {
+		t.Fatalf("Parse failure")
+	}
+
+	want := []int{0, 1, 1, 2, 1, 0, 0, 0, 0}
+
+	for i, v := range want {
+		if v != have[i] {
+			t.Fatalf("Parse(%v) == %v, want %v", ex, have, want)
+		}
+	}
+}
+
 func TestPartOne(t *testing.T) {
 	in, err := parseFish(ex)
 	if err != nil {
