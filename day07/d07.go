3bedd8efdefc7d7caf7323ef68a83b44c52b015d
Arthur E. Jones <PartyLich@gmail.com>
2022-05-16 17:38:54 -0500

    feat: add 7.1 solution

--- day07/d07.go
+++ day07/d07.go
@@ -2,23 +2,72 @@
 package day07
 
 import (
+	"math"
+	"strconv"
+
+	"github.com/partylich/advent2021/parse"
 	"github.com/partylich/advent2021/runner"
 )
 
 func parsePos(in string) ([]int, error) {
-	return []int{}, nil
+	return parse.Csv[int](in, strconv.Atoi)
+}
+
+type pos = int
+
+func dist(from, to pos) int {
+	return runner.Max(from, to) - runner.Min(from, to)
+}
+
+// cost returns the cost of moving all entries to position p
+func cost(m map[pos]int, p pos) int {
+	sum := 0
+
+	for k, w := range m {
+		if k == p {
+			continue
+		}
+
+		sum += dist(p, k) * w
+	}
+
+	return sum
 }
 
 // PartOne returns the smallest total fuel spent required to align all crabs.
 func PartOne(in []int) int {
-	return 0
+	var (
+		maxPos int
+		minPos int
+	)
+	m := make(map[pos]int)
+
+	for _, p := range in {
+		if _, ok := m[p]; !ok {
+			m[p] = 0
+		}
+
+		m[p] += 1
+
+		maxPos = runner.Max(p, maxPos)
+		minPos = runner.Min(p, minPos)
+	}
+
+	minCost := math.MaxInt
+	for pos := minPos; pos <= maxPos; pos++ {
+		if fuelc := cost(m, pos); fuelc < minCost {
+			minCost = fuelc
+		}
+	}
+
+	return minCost
 }
 
 func Solution() runner.Solution {
 	return runner.Solution{
 		Parse: func(i string) (interface{}, error) { return parsePos(i) },
 		Fn: [2]func(i interface{}) interface{}{
-			runner.Unimpl,
+			func(i interface{}) interface{} { return PartOne(i.([]int)) },
 			runner.Unimpl,
 		},
 	}
