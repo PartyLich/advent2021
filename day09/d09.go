f877616000a41e0a616b9a4b019a9c57576131f8
Arthur E. Jones <PartyLich@gmail.com>
2022-05-21 14:18:22 -0500

    feat: add 9.1 solution

--- day09/d09.go
+++ day09/d09.go
@@ -2,6 +2,7 @@
 package day09
 
 import (
+	"strconv"
 	"strings"
 
 	"github.com/partylich/advent2021/parse"
@@ -12,7 +13,7 @@ type _ParseResult = [][]string
 
 func parseLines(in string) (_ParseResult, error) {
 	lines := parse.Lines(in)
-	result := runner.NewGrid[string](len(lines), 2)
+	result := make([][]string, len(lines))
 
 	for r, l := range lines {
 		pts := strings.Split(l, "")
@@ -22,10 +23,51 @@ func parseLines(in string) (_ParseResult, error) {
 	return result, nil
 }
 
+// return true if grid[r][c] is smaller than any of its neighbors
+func smallest(grid _ParseResult, r, c int) bool {
+	width, height := len(grid[0]), len(grid)
+
+	for y := r - 1; y <= r+1; y++ {
+		if y < 0 || y >= height || (y == r) {
+			continue
+		}
+
+		if x := c; grid[r][c] >= grid[y][x] {
+			return false
+		}
+	}
+
+	for x := c - 1; x <= c+1; x++ {
+		if x < 0 || x >= width || (x == c) {
+			continue
+		}
+
+		if y := r; grid[r][c] >= grid[y][x] {
+			return false
+		}
+	}
+
+	return true
+}
+
 // PartOne returns the sum of the risk levels of all low points on your
 // heightmap.
 func PartOne(in _ParseResult) int {
 	sum := 0
+
+	// TODO: avoid checking positions whos neighbor has been determined the lowest
+	for r, row := range in {
+		for c, col := range row {
+			if smallest(in, r, c) {
+				height, err := strconv.Atoi(col)
+				if err != nil {
+					panic("unable to convert string to int")
+				}
+
+				sum += height + 1
+			}
+		}
+	}
 
 	return sum
 }
