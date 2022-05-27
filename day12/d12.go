0a3b9d580908f79fb656db30800b51b44aea657c
Arthur E. Jones <PartyLich@gmail.com>
2022-05-27 13:09:51 -0500

    feat: add 12.1 solution

--- day12/d12.go
+++ day12/d12.go
@@ -3,9 +3,11 @@ package day12
 
 import (
 	"strings"
+	"unicode"
 
 	"github.com/partylich/advent2021/parse"
 	"github.com/partylich/advent2021/runner"
+	"golang.org/x/exp/slices"
 )
 
 type _ParseResult = map[string]map[string]bool
@@ -41,17 +43,55 @@ func parseLines(in string) (_ParseResult, error) {
 	return result, nil
 }
 
+func isLower(s string) bool {
+	for _, c := range s {
+		if unicode.IsUpper(c) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func traverse(graph _ParseResult, visited []string, from string) [][]string {
+	var paths [][]string
+
+	for adj := range graph[from] {
+		// dont revisit lowercase nodes
+		if isLower(adj) && slices.Contains(visited, adj) {
+			continue
+		}
+
+		v := make([]string, len(visited), len(visited)+2)
+		copy(v, visited)
+		v = append(v, adj)
+
+		// completed path
+		if adj == "end" {
+			paths = append(paths, v)
+			continue
+		}
+
+		paths = append(paths, traverse(graph, v, adj)...)
+	}
+
+	return paths
+}
+
 // PartOne returns the number of paths from start to end, where lowercase nodes
 // may be visited multiple times
 func PartOne(in _ParseResult) int {
-	return 0
+	visited := []string{"start"}
+	paths := traverse(in, visited, "start")
+
+	return len(paths)
 }
 
 func Solution() runner.Solution {
 	return runner.Solution{
 		Parse: func(i string) (interface{}, error) { return parseLines(i) },
 		Fn: [2]func(i interface{}) interface{}{
-			runner.Unimpl,
+			func(i interface{}) interface{} { return PartOne(i.(_ParseResult)) },
 			runner.Unimpl,
 		},
 	}
