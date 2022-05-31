22be45d7e6959690a6bb060060d7b29bfc84dae2
Arthur E. Jones <PartyLich@gmail.com>
2022-05-31 15:57:22 -0500

    feat: add 14.2 solution

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Run all day's solutions:
 ```bash
 go run .
 ```
+
+I had a bad time with 14.2. Eventually I asked for help and someone told me it was just day 6 -
+which i found quite easy - in another guise. That more or less solved it for me, though I'm unsure
+why I couldn't see the connection myself.
--- day14/d14.go
+++ day14/d14.go
@@ -70,12 +70,60 @@ func PartOne(in _ParseResult) int {
 	return max - min
 }
 
+// capital letter to index
+func toIdx(c rune) int {
+	return int(c - 65)
+}
+
+// PartTwo returns the quantity of the most common element minus the quantity of
+// the least common element after 40 steps.
+func PartTwo(in _ParseResult) int {
+	pairMap := make(map[string]int)
+
+	for i := 0; i < len(in.Tmpl)-1; i++ {
+		p := in.Tmpl[i : i+2]
+		pairMap[p] += 1
+	}
+
+	for i := 0; i < 40; i++ {
+		m := make(map[string]int)
+		for k, v := range pairMap {
+			next := in.Rules[k]
+			// each insertion produces 2 new pairs
+			a := k[:1] + next
+			b := next + k[1:]
+			m[a] += v
+			m[b] += v
+		}
+		pairMap = m
+	}
+
+	var counts [26]int
+	// count the first char of each pair
+	for k, v := range pairMap {
+		counts[toIdx(rune(k[0]))] += v
+	}
+	// add the last letter of the template, the only rune that doesnt start a pair
+	counts[toIdx(rune(in.Tmpl[len(in.Tmpl)-1]))] += 1
+
+	max, min := math.MinInt, math.MaxInt
+	for _, v := range counts {
+		if v == 0 {
+			continue
+		}
+		max = runner.Max(max, v)
+		min = runner.Min(min, v)
+	}
+
+	return max - min
+}
+
 func Solution() runner.Solution {
 	return runner.Solution{
 		Parse: func(i string) (interface{}, error) { return parseLines(i) },
 		Fn: [2]func(i interface{}) interface{}{
 			func(i interface{}) interface{} { return PartOne(i.(_ParseResult)) },
-			runner.Unimpl,
+			func(i interface{}) interface{} { return PartTwo(i.(_ParseResult)) },
 		},
 	}
 }
