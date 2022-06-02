74d215bc922609824721610ee89bb3c7059062d1
Arthur E. Jones <PartyLich@gmail.com>
2022-06-01 22:58:37 -0500

    feat: add 15.1 solution

--- day15/d15.go
+++ day15/d15.go
@@ -33,14 +33,36 @@ func parseLines(in string) (_ParseResult, error) {
 // PartOne returns the lowest total risk of any path from top left to bottom
 // right.
 func PartOne(in _ParseResult) int {
-	return 0
+	width := len(in[0])
+	height := len(in)
+	result := runner.NewGrid[int](height, width)
+	result[height-1][width-1] = in[height-1][width-1]
+
+	for r := height - 1; r >= 0; r-- {
+		for c := width - 1; c > 0; c-- {
+			// last column, up only
+			if c == width-1 && r < height-1 {
+				result[r][c] = in[r][c] + result[r+1][c]
+			}
+
+			left := in[r][c-1] + result[r][c]
+			up := left
+			if r < height-1 {
+				up = in[r][c-1] + result[r+1][c-1]
+			}
+
+			result[r][c-1] = runner.Min(left, up)
+		}
+	}
+
+	return runner.Min(result[0][1], result[1][0])
 }
 
 func Solution() runner.Solution {
 	return runner.Solution{
 		Parse: func(i string) (interface{}, error) { return parseLines(i) },
 		Fn: [2]func(i interface{}) interface{}{
-			runner.Unimpl,
+			func(i interface{}) interface{} { return PartOne(i.(_ParseResult)) },
 			runner.Unimpl,
 		},
 	}
