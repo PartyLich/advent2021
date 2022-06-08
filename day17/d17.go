c5abace3482ef4e0266fa55b7c84ea0316df00d5
Arthur E. Jones <PartyLich@gmail.com>
2022-06-08 12:01:08 -0500

    feat: parse 17.1 input

--- day17/d17.go
+++ day17/d17.go
@@ -2,13 +2,41 @@
 package day17
 
 import (
+	"errors"
+	"regexp"
+	"strconv"
+
 	"github.com/partylich/advent2021/runner"
 )
 
-type _ParseResult = string
+type Bound struct {
+	Min, Max int
+}
+
+func (b Bound) InBounds(i int) bool {
+	return i <= b.Max && i >= b.Min
+}
+
+type _ParseResult = []Bound
 
 func parseLines(in string) (_ParseResult, error) {
-	return "", runner.ErrUnimplemented
+	re := regexp.MustCompile(`x=(-?\d+)\.{2}(-?\d+), y=(-?\d+)\.{2}(-?\d+)`)
+	m := re.FindSubmatch([]byte(in))
+
+	bounds := make([]int, 4)
+	for idx := 1; idx < len(m); idx++ {
+		b, err := strconv.Atoi(string(m[idx]))
+		if err != nil {
+			return nil, errors.New("parse failure")
+		}
+
+		bounds[idx-1] = b
+	}
+
+	xB := Bound{bounds[0], bounds[1]}
+	yB := Bound{bounds[2], bounds[3]}
+
+	return []Bound{xB, yB}, nil
 }
 
 // PartOne returns the highest y position of any trajectory that has a discrete
