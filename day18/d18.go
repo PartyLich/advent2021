685d437b6a482b391198b148a8f75187cf7e9b80
Arthur E. Jones <PartyLich@gmail.com>
2022-06-08 19:01:18 -0500

    feat: explode snailfish numbers

--- day18/d18.go
+++ day18/d18.go
@@ -24,11 +24,72 @@ func parseLines(in string) (_ParseResult, error) {
 }
 
 func canExplode(in SnailNum) (int, bool) {
+	open := 0
+	idx := -1
+	lastDigit := false
+	for _, r := range in {
+		switch r {
+		case '[':
+			open += 1
+			if open == 5 {
+				idx += 1
+				return idx, true
+			}
+			lastDigit = false
+		case ']':
+			open -= 1
+			lastDigit = false
+		case ',':
+			lastDigit = false
+		default:
+			if !lastDigit {
+				idx += 1
+			}
+			lastDigit = true
+		}
+	}
+
 	return 0, false
 }
 
 func explode(in SnailNum, i int) SnailNum {
-	return ""
+	if len(in) == 0 {
+		panic("explode requires valid SnailNum, received empty string")
+	}
+
+	result := string(in)
+	re := regexp.MustCompile(`(-?\d+)`)
+	digits := re.FindAllString(result, -1)
+
+	d := make([]int, len(digits))
+	for i, digit := range digits {
+		d[i], _ = strconv.Atoi(digit)
+	}
+
+	// pair's left value is added to the first regular number to the left of the
+	// exploding pair (if any)
+	if i-1 >= 0 {
+		d[i-1] += d[i]
+	}
+	// pair's right value is added to the first regular number to the right of
+	// the exploding pair (if any).
+	if i+2 < len(digits) {
+		d[i+2] += d[i+1]
+	}
+	d[i], d[i+1] = -42, -42
+
+	j := 0
+	subsDigit := func(s string) string {
+		result := fmt.Sprintf("%v", d[j])
+		j++
+
+		return result
+	}
+	result = re.ReplaceAllStringFunc(result, subsDigit)
+
+	reZero := regexp.MustCompile(`\[-42,-42\]`)
+
+	return SnailNum(reZero.ReplaceAllString(result, "0"))
 }
 
 var reSplit = regexp.MustCompile(`\d{2,}`)
