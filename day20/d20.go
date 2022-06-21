fd59ee8b408abc32d044762d86c875d04bbdaca9
Arthur E. Jones <PartyLich@gmail.com>
2022-06-21 12:20:36 -0500

    feat: parse 20.1 input

--- day20/d20.go
+++ day20/d20.go
@@ -2,17 +2,58 @@
 package day20
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/partylich/advent2021/parse"
 	"github.com/partylich/advent2021/runner"
 )
 
+type Image struct {
+	minX, minY int
+	maxX, maxY int
+	pixels     map[string]bool
+	// value of pixels outside the image focus
+	void bool
+}
+
+func NewImage() Image {
+	return Image{
+		0, 0,
+		0, 0,
+		make(map[string]bool),
+		false,
+	}
+}
+
 type _ParseResult struct {
-	algo  []string
-	image map[string]bool
+	algo  []bool
+	image Image
+}
+
+func toKey(r, c int) string {
+	return fmt.Sprintf("%v,%v", c, r)
 }
 
 func parseLines(in string) (_ParseResult, error) {
 	var result _ParseResult
+	lines := parse.Lines(in)
+
+	algoStr := lines[0]
+	algo := strings.Split(algoStr, "")
+	result.algo = make([]bool, len(algo))
+	for i, v := range algo {
+		result.algo[i] = (v == "#")
+	}
+
+	result.image = NewImage()
+	for r, l := range lines[2:] {
+		for c, pixel := range strings.Split(l, "") {
+			result.image.pixels[toKey(r, c)] = (pixel == "#")
+			result.image.maxX = runner.Max(result.image.maxX, c)
+			result.image.maxY = runner.Max(result.image.maxY, r)
+		}
+	}
 
 	return result, nil
 }
