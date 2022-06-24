1e9ba5a21f2b9913e7125e6c803a8dbea2bfb2e0
Arthur E. Jones <PartyLich@gmail.com>
2022-06-24 16:11:24 -0500

    perf: rewrite 18.1 using linked list
    Benchstat results
    name       old time/op    new time/op    delta
    PartOne-8     202ms ± 4%      16ms ±20%  -92.06%  (p=0.000 n=10+9)
    PartTwo-8     3.14s ± 6%     0.20s ±19%  -93.59%  (p=0.000 n=10+10)
    
    name       old alloc/op   new alloc/op   delta
    PartOne-8    60.0MB ± 0%    11.0MB ± 0%  -81.63%  (p=0.000 n=10+10)
    PartTwo-8     864MB ± 0%     150MB ± 0%  -82.63%  (p=0.000 n=10+10)
    
    name       old allocs/op  new allocs/op  delta
    PartOne-8      842k ± 0%      344k ± 0%  -59.11%  (p=0.000 n=10+10)
    PartTwo-8     12.5M ± 0%      4.7M ± 0%  -62.46%  (p=0.000 n=9+10)
    
    More work could maybe be done to reduce allocations.

--- day18/d18_test.go
+++ day18/d18_test.go
@@ -20,161 +20,44 @@ const ex string = `[[[0,[5,8]],[[1,7],[9,6]]],[[4,[1,2]],[[1,4],2]]]
 
 func TestPartOne(t *testing.T) {
 	t.Run("parses input", func(t *testing.T) {
-		_, err := parseLines(ex)
-		if err != nil {
+		h, err := parseLines(ex)
+		if err != nil || h == nil {
 			t.Errorf("parse failure: %v", err)
 		}
 	})
-	t.Run("explode number", func(t *testing.T) {
-		cases := []struct {
-			in   SnailNum
-			want SnailNum
-			idx  int
+
+	t.Run("parses list", func(t *testing.T) {
+		ex := `[9,[8,7]]`
+		head, err := parseNum(ex)
+		if err != nil {
+			t.Errorf("parse failure: %v", err)
+		}
+
+		h, err := parseNum("[9,1]")
+		if err != nil || h == nil {
+			t.Errorf("parse failure: %v", err)
+		}
+
+		want := []struct {
+			val, depth int
 		}{
-			{"[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]", 0},
-			{"[7,[6,[5,[4,[3,2]]]]]", "[7,[6,[5,[7,0]]]]", 4},
-			{"[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]", 3},
-			{"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", 3},
-			{"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", "[[3,[2,[8,0]]],[9,[5,[7,0]]]]", 7},
-			{
-				"[[[[4,0],[5,4]],[[7,0],[15,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]",
-				"[[[[4,0],[5,4]],[[7,0],[15,5]]],[10,[[0,[11,3]],[[6,3],[8,8]]]]]",
-				9,
-			},
+			{9, 1},
+			{8, 2},
+			{7, 2},
 		}
 
-		for _, c := range cases {
-			idx, ok := canExplode(c.in)
-			if !ok {
-				t.Errorf("canExplode(%v) \n\thave %v\n\twant %v", c.in, ok, true)
+		for i, n := 0, head; n != nil; i, n = i+1, n.right {
+			if n.val != want[i].val {
+				t.Fatalf("parseNum(%#v) \n\thave %v, \n\twant %v", ex, n.val, want[i].val)
 			}
-			if idx != c.idx {
-				t.Errorf("canExplode(%v) index \n\thave %v\n\twant %v", c.in, idx, c.idx)
-			}
-
-			have := explode(c.in, idx)
-			if have != c.want {
-				t.Errorf("explode(%v) \n\thave %v\n\twant %v", c.in, have, c.want)
-			}
-		}
-	})
-	t.Run("split number", func(t *testing.T) {
-		cases := []struct {
-			in   SnailNum
-			want SnailNum
-		}{
-			{"[[[[0,7],4],[15,[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,13]]],[1,1]]"},
-			{"[[[[0,7],4],[[7,8],[0,13]]],[1,1]]", "[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]"},
-		}
-
-		for _, c := range cases {
-
-			ok := canSplit(c.in)
-			if !ok {
-				t.Errorf("canSplit(%v) \n\thave %v\n\twant %v", c.in, ok, true)
-			}
-
-			have := split(c.in)
-			if have != c.want {
-				t.Errorf("split(%v) \n\thave %v\n\twant %v", c.in, have, c.want)
-			}
-		}
-	})
-	t.Run("reduce number", func(t *testing.T) {
-		cases := []struct {
-			in   SnailNum
-			want SnailNum
-		}{
-			{"[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]", "[[[[0,7],4],[[7,8],[6,0]]],[8,1]]"},
-			{"[[[[[9,8],1],2],3],4]", "[[[[0,9],2],3],4]"},
-			{"[[6,[5,[4,[3,2]]]],1]", "[[6,[5,[7,0]]],3]"},
-			{"[[1,2],[[3,4],5]]", "[[1,2],[[3,4],5]]"},
-		}
-
-		for _, c := range cases {
-			have := reduce(c.in)
-			if have != c.want {
-				t.Errorf("reduce(%v) \n\thave %v\n\twant %v", c.in, have, c.want)
-			}
-		}
-	})
-	t.Run("add snailfish numbers", func(t *testing.T) {
-		cases := []struct {
-			in   string
-			want SnailNum
-		}{
-			{`[1,1]
-[2,2]
-[3,3]
-[4,4]`,
-				"[[[[1,1],[2,2]],[3,3]],[4,4]]"},
-			{`[1,1]
-[2,2]
-[3,3]
-[4,4]
-[5,5]
-`,
-				"[[[[3,0],[5,3]],[4,4]],[5,5]]"},
-			{`[1,1]
-[2,2]
-[3,3]
-[4,4]
-[5,5]
-[6,6]
-`,
-				"[[[[5,0],[7,4]],[5,5]],[6,6]]"},
-			{`[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-			`,
-				"[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]"},
-			{`[[[[4,0],[5,4]],[[7,7],[6,0]]],[[8,[7,7]],[[7,9],[5,0]]]]
-[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-			`,
-				"[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]"},
-
-			{`[[[[6,7],[6,7]],[[7,7],[0,7]]],[[[8,7],[7,7]],[[8,8],[8,0]]]]
-[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]`,
-				"[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]"},
-
-			{`[[[[7,0],[7,7]],[[7,7],[7,8]]],[[[7,7],[8,8]],[[7,7],[8,7]]]]
-[7,[5,[[3,8],[1,4]]]]`,
-				"[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]"},
-
-			{`[[[[7,7],[7,8]],[[9,5],[8,7]]],[[[6,8],[0,8]],[[9,9],[9,0]]]]
-[[2,[2,2]],[8,[8,1]]]`,
-				"[[[[6,6],[6,6]],[[6,0],[6,7]]],[[[7,7],[8,9]],[8,[8,1]]]]"},
-			{`[[[0,[4,5]],[0,0]],[[[4,5],[2,6]],[9,5]]]
-[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]
-[[2,[[0,8],[3,4]]],[[[6,7],1],[7,[1,6]]]]
-[[[[2,4],7],[6,[0,5]]],[[[6,8],[2,8]],[[2,1],[4,5]]]]
-[7,[5,[[3,8],[1,4]]]]
-[[2,[2,2]],[8,[8,1]]]
-[2,9]
-[1,[[[9,3],9],[[9,0],[0,7]]]]
-[[[5,[7,4]],7],1]
-[[[[4,2],2],6],[8,7]]`,
-				"[[[[8,7],[7,7]],[[8,6],[7,7]]],[[[0,7],[6,6]],[8,7]]]"},
-		}
-
-		for _, c := range cases {
-			ns, err := parseLines(c.in)
-			if err != nil {
-				t.Fatalf("parse failure: %v", err)
-			}
-
-			have := ns[0]
-			for i := 1; i < len(ns); i++ {
-				have = add(have, ns[i])
-			}
-
-			if have != c.want {
-				t.Errorf("add(%v) \n\thave %v\n\twant %v", c.in, have, c.want)
+			if n.depth != want[i].depth {
+				t.Fatalf("parseNum(%#v) \n\thave %v, \n\twant %v", ex, n.depth, want[i].depth)
 			}
 		}
 	})
 	t.Run("calc magnitude", func(t *testing.T) {
 		cases := []struct {
-			in   SnailNum
+			in   string
 			want int
 		}{
 			{"[9,1]", 29},
@@ -189,12 +72,276 @@ func TestPartOne(t *testing.T) {
 		}
 
 		for _, c := range cases {
-			have := magnitude(c.in)
+			num, _ := parseNum(c.in)
+			have := magnitude(num)
 			if have != c.want {
 				t.Errorf("magnitude(%v) \n\thave %v\n\twant %v", c.in, have, c.want)
 			}
 		}
 	})
+	t.Run("can explode number", func(t *testing.T) {
+		cases := []struct {
+			in   string
+			want bool
+		}{
+			{"[[[[[9,8],1],2],3],4]", true},
+			{"[7,[6,[5,[4,[3,2]]]]]", true},
+			{"[[6,[5,[4,[3,2]]]],1]", true},
+			{"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]", true},
+			{"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]", true},
+			{
+				"[[[[4,0],[5,4]],[[7,0],[15,5]]],[7,[[[3,7],[4,3]],[[6,3],[8,8]]]]]",
+				true,
+			},
+		}
+
+		for _, c := range cases {
+			num, _ := parseNum(c.in)
+			_, ok := canExplode(num)
+			if !ok {
+				t.Errorf("canExplode(%v) \n\thave %v\n\twant %v", c.in, ok, true)
+			}
+		}
+	})
+	t.Run("explode number", func(t *testing.T) {
+		cases := []struct {
+			in   string
+			want []Node
+		}{
+			{"[[[[[9,8],1],2],3],4]",
+				[]Node{
+					{0, 4, nil, nil},
+					{9, 4, nil, nil},
+					{2, 3, nil, nil},
+					{3, 2, nil, nil},
+					{4, 1, nil, nil},
+				},
+			},
+			{"[7,[6,[5,[4,[3,2]]]]]",
+				[]Node{
+					{7, 1, nil, nil},
+					{6, 2, nil, nil},
+					{5, 3, nil, nil},
+					{7, 4, nil, nil},
+					{0, 4, nil, nil},
+				},
+			},
+			{"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
+
+				[]Node{
+					{3, 2, nil, nil},
+					{2, 3, nil, nil},
+					{8, 4, nil, nil},
+					{0, 4, nil, nil},
+					{9, 2, nil, nil},
+					{5, 3, nil, nil},
+					{4, 4, nil, nil},
+					{3, 5, nil, nil},
+					{2, 5, nil, nil},
+				},
+			},
+			{"[[[[0,[3,2]],[3,3]],[4,4]],[5,5]]",
+				[]Node{
+					{3, 4, nil, nil},
+					{0, 4, nil, nil},
+					{5, 4, nil, nil},
+					{3, 4, nil, nil},
+					{4, 3, nil, nil},
+					{4, 3, nil, nil},
+					{5, 2, nil, nil},
+					{5, 2, nil, nil},
+				},
+			},
+		}
+
+		for _, c := range cases {
+			num, _ := parseNum(c.in)
+			start, _ := canExplode(num)
+
+			explode(num, start)
+			for i, n := 0, num; n != nil; i, n = i+1, n.right {
+				if n.val != c.want[i].val {
+					t.Errorf("explode(%v) \n\thave %v\n\twant %v", c.in, n.val, c.want[i].val)
+				}
+				if n.depth != c.want[i].depth {
+					t.Errorf("explode(%v) \n\thave %v\n\twant %v", c.in, n.depth, c.want[i].depth)
+				}
+			}
+		}
+	})
+	t.Run("split number", func(t *testing.T) {
+		cases := []struct {
+			in   string
+			want []Node
+		}{
+			{"[[[[0,7],4],[15,[0,13]]],[1,1]]",
+				[]Node{
+					{0, 4, nil, nil},
+					{7, 4, nil, nil},
+					{4, 3, nil, nil},
+					{7, 4, nil, nil},
+					{8, 4, nil, nil},
+					{0, 4, nil, nil},
+					{13, 4, nil, nil},
+					{1, 2, nil, nil},
+					{1, 2, nil, nil},
+				},
+			},
+			{"[[[[0,7],4],[[7,8],[0,13]]],[1,1]]",
+				[]Node{
+					{0, 4, nil, nil},
+					{7, 4, nil, nil},
+					{4, 3, nil, nil},
+					{7, 4, nil, nil},
+					{8, 4, nil, nil},
+					{0, 4, nil, nil},
+					{6, 5, nil, nil},
+					{7, 5, nil, nil},
+					{1, 2, nil, nil},
+					{1, 2, nil, nil},
+				},
+			},
+		}
+
+		for _, c := range cases {
+			num, _ := parseNum(c.in)
+			start, ok := canSplit(num)
+			if !ok {
+				t.Errorf("canSplit(%v) \n\thave %v\n\twant %v", c.in, ok, true)
+			}
+
+			split(num, start)
+			for i, n := 0, num; n != nil; i, n = i+1, n.right {
+				if n.val != c.want[i].val {
+					t.Errorf("split(%v) \n\thave %v\n\twant %v", c.in, n.val, c.want[i].val)
+				}
+				if n.depth != c.want[i].depth {
+					t.Errorf("split(%v) \n\thave %v\n\twant %v", c.in, n.depth, c.want[i].depth)
+				}
+			}
+		}
+	})
+	t.Run("reduce number", func(t *testing.T) {
+		cases := []struct {
+			in   string
+			want []Node
+		}{
+			{"[[[[[4,3],4],4],[7,[[8,4],9]]],[1,1]]",
+				[]Node{
+					{0, 4, nil, nil},
+					{7, 4, nil, nil},
+					{4, 3, nil, nil},
+					{7, 4, nil, nil},
+					{8, 4, nil, nil},
+					{6, 4, nil, nil},
+					{0, 4, nil, nil},
+					{8, 2, nil, nil},
+					{1, 2, nil, nil},
+				},
+			},
+			{"[[[[[9,8],1],2],3],4]",
+				[]Node{
+					{0, 4, nil, nil},
+					{9, 4, nil, nil},
+					{2, 3, nil, nil},
+					{3, 2, nil, nil},
+					{4, 1, nil, nil},
+				},
+			},
+			{"[[6,[5,[4,[3,2]]]],1]",
+				[]Node{
+					{6, 2, nil, nil},
+					{5, 3, nil, nil},
+					{7, 4, nil, nil},
+					{0, 4, nil, nil},
+					{3, 1, nil, nil},
+				},
+			},
+			{"[[1,2],[[3,4],5]]",
+				[]Node{
+					{1, 2, nil, nil},
+					{2, 2, nil, nil},
+					{3, 3, nil, nil},
+					{4, 3, nil, nil},
+					{5, 2, nil, nil},
+				},
+			},
+		}
+
+		for _, c := range cases {
+			num, _ := parseNum(c.in)
+
+			have := reduce(num)
+			for i, n := 0, have; n != nil; i, n = i+1, n.right {
+				if n.val != c.want[i].val {
+					t.Errorf("reduce(%v) val \n\thave %v\n\twant %v", c.in, n.val, c.want[i].val)
+				}
+				if n.depth != c.want[i].depth {
+					t.Errorf("reduce(%v) depth \n\thave %v\n\twant %v", c.in, n.depth, c.want[i].depth)
+				}
+			}
+		}
+	})
+	t.Run("add snailfish numbers", func(t *testing.T) {
+		cases := []struct {
+			in   string
+			want []Node
+		}{
+			{`[1,1]
+[2,2]
+[3,3]
+[4,4]`,
+				[]Node{
+					{1, 4, nil, nil},
+					{1, 4, nil, nil},
+					{2, 4, nil, nil},
+					{2, 4, nil, nil},
+					{3, 3, nil, nil},
+					{3, 3, nil, nil},
+					{4, 2, nil, nil},
+					{4, 2, nil, nil},
+				},
+			},
+			{`[1,1]
+[2,2]
+[3,3]
+[4,4]
+[5,5]`,
+				[]Node{
+					{3, 4, nil, nil},
+					{0, 4, nil, nil},
+					{5, 4, nil, nil},
+					{3, 4, nil, nil},
+					{4, 3, nil, nil},
+					{4, 3, nil, nil},
+					{5, 2, nil, nil},
+					{5, 2, nil, nil},
+				},
+			},
+		}
+
+		for _, c := range cases {
+			ns, err := parseLines(c.in)
+			if err != nil {
+				t.Fatalf("parse failure: %v", err)
+			}
+
+			have := ns[0]
+			for i := 1; i < len(ns); i++ {
+				have = add(have, ns[i])
+			}
+
+			for i, n := 0, have; n != nil; i, n = i+1, n.right {
+				if n.val != c.want[i].val {
+					t.Errorf("add(%v) val \n\thave %v\n\twant %v", c.in, n.val, c.want[i].val)
+				}
+				if n.depth != c.want[i].depth {
+					t.Errorf("add(%v) depth \n\thave %v\n\twant %v", c.in, n.depth, c.want[i].depth)
+				}
+			}
+		}
+	})
+
 	t.Run("executes task", func(t *testing.T) {
 		in, err := parseLines(ex)
 		if err != nil {
